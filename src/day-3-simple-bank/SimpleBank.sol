e5dfd1716e02f77e3a0fcebaf69145d65f7e9921
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2025-08-14 00:56:26 +0530

    forge fmt
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- src/day-3-simple-bank/SimpleBank.sol
+++ src/day-3-simple-bank/SimpleBank.sol
@@ -15,7 +15,7 @@ contract SimpleBank {
     function withdraw(uint256 amount) public {
         require(balances[msg.sender] >= amount, "Insufficient balance");
         balances[msg.sender] -= amount;
-        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        (bool success,) = payable(msg.sender).call{value: amount}("");
         require(success, "Transfer failed");
     }
 }
